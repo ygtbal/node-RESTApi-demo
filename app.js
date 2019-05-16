de35794c10e61261cbf78a02c8506dbe7b205b6e
yigitbalalan <yigitbalalan@gmail.com>
2019-05-16 11:48:03 +0300

    node restful api first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://yigit:1994yigit@node-rest-shop-api-pnsxi.mongodb.net/test?retryWrites=true' +
+                    {
+                      useMongoClient: true,
+                    },
+                    {
+                      useNewUrlParser: true,
+                    }
+                  ) 
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Resolving Cors Problems
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});  
+  }
+  next();
+})
+
+// Routes which should handle request
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+//Handling errors
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app; 
